8614e610b89942abeb5ca8826ff028fd9ef54218
Guilherme Cesário <guilhermecesario1808@gmail.com>
2026-01-16 16:33:42 -0300

    correção de ambiente

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,34 @@
 require("dotenv").config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 module.exports = {
   development: {
-    client: process.env.DB_CLIENT || "pg",
+    client: "pg",
     connection: {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: "./src/migrations",
+    },
+    seeds: {
+      directory: "./src/seeds",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: { rejectUnauthorized: false },
     },
     migrations: {
       directory: "./src/migrations",
